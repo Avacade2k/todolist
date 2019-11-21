baf1f97bafdc2bcc4eb2d23c1a7ad309785a4ead
Don-Cheadle <alexk753@gmail.com>
2019-11-21 10:59:44 +0100

    fixed clicking to check off list items cleaned up some code

--- scripts.js
+++ scripts.js
@@ -1,62 +1,51 @@
 var list = [];
 var node;
-var textnode; 
+var textnode;
 var red;
 var green;
 var blue;
 
 
-function handle(e){
-        if(e.keyCode === 13){
-            e.preventDefault();
-            addThing();
-        }
+function handle(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        addThing();
     }
+}
 
 function switcheroo() {
-    red = Math.floor(Math.random()*256);
-    green = Math.floor(Math.random()*256);
-    blue = Math.floor(Math.random()*256);
-    document.getElementById("blyat").style.backgroundColor = "rgb("+red+","+green+","+blue+")";
-    document.getElementById("trash").style.backgroundColor = "rgb("+blue+","+red+","+green+")";
-    document.getElementById("addButton").style.backgroundColor = "rgb("+red+","+green+","+blue+")";
-    document.getElementById("Switch").style.backgroundColor = "rgb("+blue+","+red+","+green+")";
-    }
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    document.getElementById("blyat").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.getElementById("trash").style.backgroundColor = "rgb(" + blue + "," + red + "," + green + ")";
+    document.getElementById("addButton").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.getElementById("Switch").style.backgroundColor = "rgb(" + blue + "," + red + "," + green + ")";
+}
 
 function del() {
     $(".checked").remove();
 }
 
-function cross(){
-  $("li").toggleClass( "checked" );
-}
-
 function addThing() {
-    if(addItem.value != ""){
+    if (addItem.value != "") {
         list.push(document.getElementById("addItem").value);
         console.log(list);
-        textnode = document.createTextNode(list[list.length-1]);
-        node = document.createElement("LI");
+        textnode = document.createTextNode(list[list.length - 1]);
+        node = document.createElement("li");
         node.appendChild(textnode);
         document.getElementById("sortable").appendChild(node);
         document.getElementById("addItem").value = "";
-        node.setAttribute("onclick", "cross()")
-       }
-    else{
+        $("li").last().click(function () {
+        $(this).toggleClass("checked");
+        });
+    } else {
         alert("Input cannot be empty!");
     }
 }
 
-$( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-    $( "#sortable" ).sortable( "option", "axis", "y" );
-  } );
-
-$(document).ready(function(){
-    $("li").on({
-        'click': function(){
-        $(this).css({"text-decoration": "overline"});
-        }
-    });    
+$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+    $("#sortable").sortable("option", "axis", "y");
 });
